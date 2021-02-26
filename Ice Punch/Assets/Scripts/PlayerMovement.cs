0217c0894f74561c626bf57d87cda5d00a6a7dde
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-02-26 12:45:20 -0500

    Character Movement Update

---
+++ Ice Punch/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector2 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        Debug.Log(change);
+    }
+}
