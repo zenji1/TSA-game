e619b0acb3cb509d90ee972dc58fb03142d2b0da
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-03-12 23:00:57 -0500

    basic movement

--- Ice Punch/Assets/Scripts/PlayerMovement.cs
+++ Ice Punch/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D myRigidBody;
-    private Vector2 change;
+    private Vector3 change;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +17,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        change = Vector2.zero;
+        change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        Debug.Log(change);
+        if(change != Vector3.zero);
+        {
+            MoveCharacter();
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
     }
 }
