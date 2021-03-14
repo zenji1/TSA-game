834d77ab406fb83d871fb5bca5e4ecacb0cbb673
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-03-14 19:34:23 -0400

    Animation

--- Ice Punch/Assets/Scripts/PlayerMovement.cs
+++ Ice Punch/Assets/Scripts/PlayerMovement.cs
@@ -5,29 +5,39 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    private Rigidbody2D myRigidBody;
+    private Rigidbody2D myRigidbody;
     private Vector3 change;
-
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        change = Vector3.zero;
+        change = Vector2.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if(change != Vector3.zero);
-        {
+        UpdateAnimationAndMove();
+        
+    }
+    void UpdateAnimationAndMove(){
+        if (change != Vector3.zero){
             MoveCharacter();
+            animator.SetFloat("move X", change.x);
+            animator.SetFloat("move Y", change.y);
+            animator.SetBool("moving", true);
+        } 
+        else {
+            animator.SetBool("moving", false);
         }
     }
-
     void MoveCharacter()
     {
-        myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
     }
 }
