48b2ce8cd2e559d94ea983465d364463a89c825e
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-03-16 15:20:33 -0400

    AttackAnimations

--- Ice Punch/Assets/Scripts/PlayerMovement.cs
+++ Ice Punch/Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
@@ -11,20 +18,39 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
-        
+        animator.SetFloat("move X", 0);
+        animator.SetFloat("move Y", -1);
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        change = Vector2.zero;
+        change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
-        
+        if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
     }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.33f);
+        currentState = PlayerState.walk;
+    }
+
     void UpdateAnimationAndMove(){
         if (change != Vector3.zero){
             MoveCharacter();
