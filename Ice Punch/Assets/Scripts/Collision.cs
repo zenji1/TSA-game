50b328b074fa90686f7d90a1108bd3b76f46bb6a
kirikaze24 <kirikaze42@gmail.com>
2021-03-18 04:22:15 -0400

    map, working on collision

---
+++ Ice Punch/Assets/Scripts/Collision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+    public Vector2 cameraChange;
+    public Vector3 playerChange;
+     public Vector2 cameraChangeMax;
+     public Vector2 cameraChangeMin;
+    private CameraMovement cam; 
+
+    // Start is called before the first frame update
+    void Start() {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+      
+    }
+
+    private void OnTriggerEnter2d(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            cam.minPosition.x = cameraChangeMin.x;
+            cam.minPosition.y = cameraChangeMin.y;
+            cam.maxPosition.x = cameraChangeMax.x;
+            cam.maxPosition.y = cameraChangeMax.y;
+            other.transform.position += playerChange;
+        }
+    }
+}
