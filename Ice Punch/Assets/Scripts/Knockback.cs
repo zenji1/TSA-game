835d94b7d57feb9b659b78188b598a192ae5981e
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-03-20 00:30:04 -0400

    Updates RigidBody

--- Ice Punch/Assets/Scripts/Knockback.cs
+++ Ice Punch/Assets/Scripts/Knockback.cs
@@ -14,11 +14,11 @@ public class Knockback : MonoBehaviour
             Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
             if(enemy != null)
             {
-                enemy.isKinematic = false;
                 Vector2 difference = enemy.transform.position - transform.position;
                 difference = difference.normalized * thurst;
                 enemy.AddForce(difference, ForceMode2D.Impulse);
                 StartCoroutine(KnockCo(enemy));
+                Debug.Log("coroutine");
             }
         }
     }
@@ -28,7 +28,6 @@ public class Knockback : MonoBehaviour
         {
             yield return new WaitForSeconds(knockTime);
             enemy.velocity = Vector2.zero;
-            enemy.isKinematic = true;
         }
     }
 }
