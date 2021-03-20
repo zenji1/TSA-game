d96cc8c48439934ae87c44d9dd580bdc9ef2a681
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-03-20 01:20:59 -0400

    Enemy changes along with Knock

--- Ice Punch/Assets/Scripts/Mob2.cs
+++ Ice Punch/Assets/Scripts/Mob2.cs
@@ -2,29 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mob2 : Enemy
-{
+public class Mob2 : Enemy {
+
+    private Rigidbody2D myRigidbody;
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = GameObject.FindWithTag("Player").transform;
-        
-    }
+    public Animator anim;
 
-    // Update is called once per frame
-    void Update()
-    {
+
+	// Use this for initialization
+	void Start () {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
         CheckDistance();
-    }
+	}
+
     void CheckDistance()
     {
-        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
+        if(Vector3.Distance(target.position, 
+                            transform.position) <= chaseRadius
+           && Vector3.Distance(target.position,
+                               transform.position) > attackRadius)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk
+                && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+            }
+        }
+    }
+
+    private void ChangeState(EnemyState newState){
+        if(currentState != newState)
+        {
+            currentState = newState;
         }
     }
 }
