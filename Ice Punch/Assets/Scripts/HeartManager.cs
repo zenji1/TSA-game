724b1377888475df17127c0aeb112e2abd0846a5
Noah Dillard <45115016+ProfessorN5@users.noreply.github.com>
2021-03-22 18:09:45 -0400

    error fixed

--- Ice Punch/Assets/Scripts/HeartManager.cs
+++ Ice Punch/Assets/Scripts/HeartManager.cs
@@ -19,7 +19,7 @@ public class HeartManager : MonoBehaviour {
 
     public void InitHearts()
     {
-        for (int i = 0; i < heartContainers.RuntimeValue; i ++)
+        for (int i = 0; i < Mathf.Min(hearts.Length, heartContainers.initialValue); i++)
         {
             hearts[i].gameObject.SetActive(true);
             hearts[i].sprite = fullHeart;
@@ -30,7 +30,7 @@ public class HeartManager : MonoBehaviour {
     {
         InitHearts();
         float tempHealth = playerCurrentHealth.RuntimeValue / 2;
-        for (int i = 0; i < heartContainers.RuntimeValue; i ++)
+        for (int i = 0; i < Mathf.Min(hearts.Length, heartContainers.initialValue); i++)
         {
             if(i <= tempHealth-1)
             {
